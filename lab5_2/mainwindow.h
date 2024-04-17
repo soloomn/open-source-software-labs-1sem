3aba603525cb7dae47b8e350ff5d4dad8d94dcbd
Tim <resn2p@gmail.com>
2024-04-18 00:00:59 +0300

    Create mainwindow.h

---
+++ lab5_2/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QString text;
+    QFile file;
+
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionSymbol_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
