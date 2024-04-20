feb0224013c15c647ce77cfbf36c2aacc1bae165
Tim <resn2p@gmail.com>
2024-04-21 00:45:55 +0300

    Create circle.h

---
+++ lab7_3/circle.h
@@ -0,0 +1,20 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include <QtGui>  // обеспечивает графическую систему
+class Circle
+{
+
+public:
+    QPoint p; // координаты центра окружности
+    int r; // радиус
+    QColor color; // цвет
+
+    Circle(); // конструктор по умолчанию
+    Circle(QPoint& _p, int _r, QColor col); // основной конструктор
+    Circle(const Circle& ); // конструктор копирования
+
+    QPoint* drstar(QPoint);
+    Circle& operator = (const Circle& ); // оператор присваивания
+
+};
+#endif // CIRCLE_H
