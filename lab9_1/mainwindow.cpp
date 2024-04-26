26c576b8a0254caa6e819efcc70b408c63cee589
Tim <resn2p@gmail.com>
2024-04-26 22:56:13 +0300

    Create mainwindow.cpp

---
+++ lab9_1/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "pole.h"
+#include <QScrollArea>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QScrollArea* scroll = new QScrollArea(this); // создаем область прокрутки
+    Pole* pole = new Pole(scroll);  // создаем новый виджет
+    scroll->setWidget(pole);  // размещаем виджет в области прокрутки
+    setCentralWidget(scroll);  // делаем область прокрутки рабочей областью окна
+    connect(ui->actionOpen, SIGNAL(triggered()), pole, SLOT(fileOpen())); // связываем вызов команды File|Open со слотом, который мы создадим в классе Pole
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
