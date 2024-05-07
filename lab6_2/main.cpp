8cc7f277f675c51921bb7a5a0cdec5ccab0c6d20
Tim <resn2p@gmail.com>
2024-05-07 15:53:47 +0300

    Update main.cpp

--- lab6_2/main.cpp
+++ lab6_2/main.cpp
@@ -1,13 +1,12 @@
 #include "mainwindow.h"
 
-#include <QtGui>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.resize(400,400); // задаем размер окна
+    w.resize(600,600); // задаем размер окна
     w.move(450,250); // перемещаем окно в центр экрана
     w.show();
     return a.exec();
